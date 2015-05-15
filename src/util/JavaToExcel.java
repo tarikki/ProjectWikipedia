a1af3e84dc9e19837bf5508a10ec0cb16c709d58
Pepe Lehtonen <pepe.lehtonen@gmail.com>
2015-05-15 10:53:16 +0200

    Minor fixes & code cleanup for JavaToExcel

--- src/util/JavaToExcel.java
+++ src/util/JavaToExcel.java
@@ -13,6 +13,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.format.DateTimeFormat;
@@ -31,68 +32,75 @@ import java.util.Iterator;
  * Created by Pepe on 8.5.2015.
  */
 public class JavaToExcel {
+    private GraphIO graphIO;
+    private String timestamp;
+    private DateFormat dateFormat;
+    private org.joda.time.format.DateTimeFormatter fmt;
 
-    // TODO decide what to use (HSSF / XSSF) Need to do testing with bigger datasets.
+    private DateTime start;
+    private DateTime end;
 
-    public void writeExcel(String graphLocation) throws IOException {
+    private XSSFWorkbook wb;
+    private ArrayList<XSSFSheet> sheets;
+    private static final int MAX_DISTANCE = 2;
 
-        int maxDistance = 2;
-        ArrayList<Node> nodesToProcess;
-
-        GraphIO graphIO = new GraphIO();
-        Graph graph = graphIO.loadGraph(graphLocation);
-        nodesToProcess = graph.getNodes();
-
-        int highest = 0;
-
-
-        for (Node node : nodesToProcess) {
-
-            if (node.getDistanceFromStart() > highest) {
-                highest = node.getDistanceFromStart();
-            }
-        }
-
-
-        /// Timestamp for filename
-        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
+    public JavaToExcel() {
+        graphIO = new GraphIO();
+        dateFormat = new SimpleDateFormat("dd-MM-yyyy HH-mm-ss");
         Date date = new Date();
-        String timestamp = dateFormat.format(date);
+        timestamp = dateFormat.format(date);
+        fmt = DateTimeFormat.forPattern("dd-MM-yyyy");
+    }
 
-        org.joda.time.format.DateTimeFormatter fmt = DateTimeFormat.forPattern("dd-MM-yyyy");
 
-        String fileName = FilePaths.EXCEL_FILES + graph.getStartingArticle() + " " + timestamp + ".xlsx" + FilePaths.osPathCorrection();    //name and location of Excel file
-
+    public void createWorkBook() {
+        wb = new XSSFWorkbook();
+        sheets = new ArrayList<>();
 
         /// Stuff for the excel file
         String sheetName = "";    //name of Sheet of excel file
 
-        ArrayList<XSSFSheet> sheets = new ArrayList<>();
-
-        XSSFWorkbook wb = new XSSFWorkbook();
 
         /// Create sheets.
-        for (int i = 1; i < maxDistance + 1; i++) {
-            sheetName = "Sheet with hop count " + i;
+        for (int i = 1; i < MAX_DISTANCE + 1; i++) {
+            sheetName = "Articles " + i + " hop(s) from start";
             XSSFSheet sheet = wb.createSheet(sheetName);
             sheets.add(sheet);
 
         }
+    }
 
-        System.out.println(sheets.size());
-        DateTime start = graph.getJodaStartDate();
-        DateTime end = graph.getJodaEndDate();
+    public void autoSizeArticleNameColumn() {
+        for (XSSFSheet sheet : sheets) {
+            sheet.autoSizeColumn(0);
+        }
+    }
+
+
+    public void writeExcel(String graphLocation) throws IOException {
+
+
+        createWorkBook();
+
+        ArrayList<Node> nodesToProcess;
+
+        graphIO = new GraphIO();
+        Graph graph = graphIO.loadGraph(graphLocation);
+        nodesToProcess = graph.getNodes();
+
+
+        String fileName = FilePaths.EXCEL_FILES + graph.getStartingArticle() + " " + timestamp + ".xlsx" + FilePaths.osPathCorrection();    //name and location of Excel file
 
 
         System.out.println(nodesToProcess.size() + " nodes to process.");
         System.out.println("Starting to write the Excel file..");
 
+        start = graph.getJodaStartDate();
+        end = graph.getJodaEndDate();
 
         int daysBetween = Days.daysBetween(start.toLocalDate(), end.toLocalDate()).getDays();
 
         // Y-axis. Populates fields with dates in range.
-
-
         for (XSSFSheet sheet : sheets) {
             Row titleRow = sheet.createRow(0);
 
@@ -105,24 +113,24 @@ public class JavaToExcel {
             start = graph.getJodaStartDate();
         }
 
-int c1 = 1;
+        // Sheet row counters.
+        int c1 = 1;
         int c2 = 1;
+
         // X-axis. Populates field with article names.
         for (int i = 1; i < nodesToProcess.size(); i++) {
             Node node = nodesToProcess.get(i - 1);
 
 
-            if (node.getDistanceFromStart()==1) {
+            if (node.getDistanceFromStart() == 1) {
                 Row r = sheets.get(0).createRow(c1);
                 Cell cell = r.createCell(0);
                 cell.setCellValue(nodesToProcess.get(i).getArticleName());
-            c1++;
-            }
-
-              else{
-                    Row r1 = sheets.get(1).createRow(c2);
-                    Cell cell1 = r1.createCell(0);
-                    cell1.setCellValue(nodesToProcess.get(i).getArticleName());
+                c1++;
+            } else {
+                Row r1 = sheets.get(1).createRow(c2);
+                Cell cell1 = r1.createCell(0);
+                cell1.setCellValue(nodesToProcess.get(i).getArticleName());
                 c2++;
 
 
@@ -133,38 +141,38 @@ int c1 = 1;
 
 
         // Loop that populates fields with view count for each article
-        int counter = 1;
+
+        int counter = 1; /// Counter for days.
 
         for (DateTime currentDate = graph.getJodaStartDate(); !currentDate.isAfter(end); currentDate = currentDate.plusDays(1)) {
             System.out.println("Going through date: " + currentDate);
 
+            /// Sheet row counters
             int x1 = 1;
             int x2 = 1;
+
             for (int i = 1; i < nodesToProcess.size(); i++) {
                 Node node = nodesToProcess.get(i - 1);
 
-                switch (node.getDistanceFromStart()) {
-
-                    case 1:
-                        Row row = sheets.get(0).getRow(x1);
+                if (node.getDistanceFromStart() == 1) {
+                    Row row = sheets.get(0).getRow(x1);
 
 
-                        if (row == null) {
-                            row = sheets.get(0).createRow(x1 + 1);
-                        }
-                        row.createCell(counter).setCellValue(node.getViewCountForDay(currentDate));
-                        x1++;
-                        break;
-                    case 2:
-                        Row row1 = sheets.get(1).getRow(x2);
-                        if (row1 == null) {
-                            row1 = sheets.get(1).createRow(x2 + 1);
-                        }
-                        row1.createCell(counter).setCellValue(node.getViewCountForDay(currentDate));
-                        x2++;
-                        break;
-                    default:
-                        break;
+                    if (row == null) {
+                        row = sheets.get(0).createRow(x1 + 1);
+                    }
+                    row.createCell(counter).setCellValue(node.getViewCountForDay(currentDate));
+                    x1++;
+                }
+
+                else {
+                    Row row1 = sheets.get(1).getRow(x2);
+                    if (row1 == null) {
+                        row1 = sheets.get(1).createRow(x2 + 1);
+                    }
+                    row1.createCell(counter).setCellValue(node.getViewCountForDay(currentDate));
+                    x2++;
+
                 }
 
 
@@ -173,19 +181,14 @@ int c1 = 1;
         }
 
 
-        for (XSSFSheet sheet : sheets)
-        {
-            sheet.autoSizeColumn(0);
-        }
+        autoSizeArticleNameColumn();
 
-            //Write the workbook to an output stream.
-            FileOutputStream finalExcel = new FileOutputStream(fileName);
-            wb.write(finalExcel);
-            System.out.println("Successfully saved the Excel to: " + fileName);
-            finalExcel.flush();
-            finalExcel.close();
-
-
+        //Write the workbook to an output stream.
+        FileOutputStream finalExcel = new FileOutputStream(fileName);
+        wb.write(finalExcel);
+        System.out.println("Successfully saved the Excel to: " + fileName);
+        finalExcel.flush();
+        finalExcel.close();
 
 
     }
